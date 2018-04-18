5c1e0b1fe0b6f2e7b850cf66f5630f341eee77bc
JustKirill1 <mkir05@mail.ru>
2018-04-18 09:18:56 -0700

    test

---
+++ lesson3/amount.rb
@@ -0,0 +1,12 @@
+puts " Write 1.long"
+p "2. breadth"
+p "3 high"
+
+a = gets.chomp
+b = gets.chomp
+c = gets.chomp
+a1 = a.to_i
+b1 = b.to_i
+c1 = c.to_i
+e = a1*b1*c1
+p "Amount of ur figure is " + e.to_s
--- lesson3/telephone.rb
+++ lesson3/telephone.rb
@@ -1,6 +1,8 @@
-def what
+
+def telephone
   puts "Whats your number ?"
   tnumber = gets.chomp
-  puts "Length of your number is " + tnumber.length.to_s + " Your last number is "
+  tnumber1 = tnumber[-1, 1]
+  puts "Length of your number is " + tnumber.length.to_s + " Your last number is " + tnumber1.to_s
 end
-what
+telephone
