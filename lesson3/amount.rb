216d2dcc8e1656ae467829bda93cd1350d82e288
JustKirill1 <mkir05@mail.ru>
2018-04-19 05:34:19 -0700

    test

--- lesson3/amount.rb
+++ lesson3/amount.rb
@@ -1,12 +1,15 @@
-puts " Write 1.long"
-p "2. breadth"
-p "3 high"
+def amount
+  puts " Write 1.long"
+  p "2. breadth"
+  p "3 high"
 
-a = gets.chomp
-b = gets.chomp
-c = gets.chomp
-a1 = a.to_i
-b1 = b.to_i
-c1 = c.to_i
-e = a1*b1*c1
-p "Amount of ur figure is " + e.to_s
+  a = gets.chomp
+  b = gets.chomp
+  c = gets.chomp
+  a1 = a.to_i
+  b1 = b.to_i
+  c1 = c.to_i
+  e = a1*b1*c1
+  p "Amount of ur figure is " + e.to_s
+end
+amount
