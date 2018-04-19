9856ba40dad365238366654c17d7825f75731041
JustKirill1 <mkir05@mail.ru>
2018-04-19 05:29:14 -0700

    lol

--- lesson3/catsdogs.rb
+++ lesson3/catsdogs.rb
@@ -1,6 +1,6 @@
 puts "Write years and i will give u this years of dogs and cats"
 vozrastd = gets.chomp
-vozrastd1 = vozrastd.to_i*15+vozrastd.to_i*9+vozrastd.to_i*5
-vozrastc = vozrastd.to_i*15+vozrastd.to_i*9+vozrastd.to_i*4
+vozrastd1 = vozrastd.to_i-2+15+9+vozrastd.to_i*5
+vozrastc = vozrastd.to_i-2+15+9+vozrastd.to_i*4
 puts "Dogs are " + vozrastd1.to_s + " years"
 puts "And cats are " + vozrastc.to_s + " years"
